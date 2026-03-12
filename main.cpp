ce72dc43c8c35bbe9f86a27eae9a22722e09ffc2
IlIXXIlI <127801939+IlIXXIlI@users.noreply.github.com>
2026-03-12 17:34:18 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,30 +8,73 @@ double func(double x) {
 }
 
 
-double a(Func f, std::pair<double, double> limit, int step=10) { 
-    double h = (limit.second - limit.first) / step; 
-
-    double result = 0;
-    for (int i = 0; i < step; i++) {
-        result += h * f(limit.first + h * (i + 0.5));
-    }
-    return result;
-}
-
-double b(Dataset dataset, std::pair<double, double> limit) {
+double square(Dataset& dataset, std::pair<double, double> limit) {
     double h = dataset[1].first - dataset[0].first;
     double a = limit.first;
     double b =  limit.second;
     double result = 0;
-    for (int i = 0; data[i].first < b; ++i) {
-        std::cout << h << std::endl;
+    
+    for (int i = 0; dataset[i].first < b; ++i) {
+        if (dataset[i].first < a) continue; 
+
         result += dataset[i].second;
     }
     return result * h;
 }
+
+
+double trapezoid(Dataset& dataset, std::pair<double, double> limit) {
+    double h = dataset[1].first - dataset[0].first;
+    double a = limit.first;
+    double b =  limit.second;
+    double result = 0;
+    
+    for (int i = 0; dataset[i].first < b; ++i) {
+        if (dataset[i].first < a) continue; 
+        result += ((dataset[i + 1].second + dataset[i].second) / 2);
+    }
+    return result * h;
+}
+double simpson(Dataset& dataset, std::pair<double, double> limit) {
+    double h = dataset[1].first - dataset[0].first;
+    double a = limit.first;
+    double b =  limit.second;
+    double result = 0;
+    for (int i = 1; dataset[i].first <= b; ++i) {
+        if (dataset[i].first == a) result += dataset[i].second;
+        if (dataset[i].first == b) result += dataset[i].second;
+    } 
+    for (int i = 1; dataset[i].first < b; ++i) {
+        if (dataset[i].first <= a) continue; 
+        int k = (i % 2 == 0) ? 2 : 4;
+        result += dataset[i].second * k;
+    }
+    return result * h / 3;
+}
+
+#if 0
+double cheb(Func& f, std::pair<double, double> limit) {
+    double h = dataset[1].first - dataset[0].first;
+    double a = limit.first;
+    double b =  limit.second;
+    double result = 0;
+    for (int i = 1; dataset[i].first <= b; ++i) {
+        if (dataset[i].first == a) result += dataset[i].second;
+        if (dataset[i].first == b) result += dataset[i].second;
+    } 
+    for (int i = 1; dataset[i].first < b; ++i) {
+        if (dataset[i].first <= a) continue; 
+        int k = (i % 2 == 0) ? 2 : 4;
+        result += dataset[i].second * k;
+    }
+    return result * h / 3;
+}
+#endif
 int main() {
-    auto dataset = parseCvv("/Users/dima.../CLionProjects/LAB_3/dataset_02.csv");
-    std::cout << a(func, std::make_pair(0.1, 0.6), 30) << std::endl;
-    std::cout << b(dataset, std::make_pair(0.1, 0.6));
+    Dataset dataset = parseCvv("./data/dataset_02.csv");
+    
+    std::cout << square(dataset, std::make_pair(0.1, 0.6)) << std::endl;;
+    std::cout << trapezoid(dataset, std::make_pair(0.1, 0.6)) << std::endl;
+    std::cout << simpson(dataset, std::make_pair(0.1, 0.6)) << std::endl;
     return 0;
 }
